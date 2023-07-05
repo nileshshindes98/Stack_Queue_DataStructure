21540cdbf89e93542b24f44da0ef6cc1b4a22e34
Nilesh Shinde <nileshshindes98@gmail.com>
2023-07-05 22:44:55 +0530

    Added : create stack with value

--- StackQueueDataStructure.java
+++ StackQueueDataStructure.java
@@ -1,7 +1,25 @@
 package stack;
 
+import java.util.Stack;
 public class StackQueueDataStructure {
     public static void main(String[] args) {
-        System.out.println("*****stack****");
+        stack();
+
+    }
+
+    public static void stack() {
+        System.out.println("************ Stack *************");
+        System.out.println();
+        Stack<Integer> stack = new Stack<>();
+
+        //UC1=Ability to create a Stack of 56->30->70
+        stack.push(56);
+        stack.push(30);
+        stack.push(70);
+
+        System.out.println("Stack = " +stack);
+        
+        System.out.println();
+        System.out.println("************ Stack *************");
     }
 }
