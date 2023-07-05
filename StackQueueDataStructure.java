480cbd2268bbded9ed209c94f50a02a159ca3837
Nilesh Shinde <nileshshindes98@gmail.com>
2023-07-05 23:30:05 +0530

    Added:Ability to peak and pop from the Stack till it is empty 56->30->70

--- StackQueueDataStructure.java
+++ StackQueueDataStructure.java
@@ -6,7 +6,6 @@ public class StackQueueDataStructure {
         stack();
 
     }
-
     public static void stack() {
         System.out.println("************ Stack *************");
         System.out.println();
@@ -18,7 +17,39 @@ public class StackQueueDataStructure {
         stack.push(70);
 
         System.out.println("Stack = " +stack);
-        
+
+        //UC2-Ability to peak and pop from the Stack till it is empty
+        //peak stack 1st elemnt
+        int peak1 = stack.peek();
+        System.out.println("Peek element = " +peak1);
+
+        //pop the stack 1st elemnt
+        int pop1 = stack.pop();
+        System.out.println("Poped element = " +pop1);
+
+        //peak stack 2nd element
+        int peak2 = stack.peek();
+        System.out.println("Peek element = " +peak2);
+
+        //pop the stack 2nd element
+        int pop2 = stack.pop();
+        System.out.println("Poped element = " +pop2);
+
+
+        //peak stack 3rd element
+        int peak3 = stack.peek();
+        System.out.println("Peek element = " +peak3);
+
+        //pop the stack 3rd element
+        int pop3 = stack.pop();
+        System.out.println("Poped element = " +pop3);
+
+
+        System.out.println();
+        //sout stack list after pop
+        System.out.println("Stack After POP = " +stack);
+
+
         System.out.println();
         System.out.println("************ Stack *************");
     }
